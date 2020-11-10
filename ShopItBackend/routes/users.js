54d721274a5451f2adeb5e4cf2f47500a8addf68
dvorjackz <dvorjackz@gmail.com>
2020-11-09 16:44:51 -0800

    Introductory models for item, sector, and user

--- ShopItBackend/routes/users.js
+++ ShopItBackend/routes/users.js
@@ -3,20 +3,30 @@ const express = require('express');
 const router = express.Router();
 let User = require('../models/user.model');
 
+// Return all users
 router.route('/').get((req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Add new user to database
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
+    const { username, admin } = req.body;
 
-    const newUser = new User( {"username": username} );
+    const newUser = new User( {"username": username, "admin": admin} );
 
     newUser.save()
         .then(() => res.json("User " + username + " was added!"))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/delete').delete((req, res) => {
+    const username = req.body.username;
+
+    User.findOneAndDelete({ username: { $eq : username } })
+        .then(() => res.json(`${username} has been deleted!`))
+        .catch(err => res.status(400).json(`Error: Unable to delete ${username}.`));
 });
 
 module.exports = router;
