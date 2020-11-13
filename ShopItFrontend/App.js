2a641a0c3cc81c818a4b9607cc1ba15440741829
Maggie Shi <maggieshi6@gmail.com>
2020-11-12 17:58:23 -0800

    created GroceryStoreSearch, added basic functionality

--- ShopItFrontend/App.js
+++ ShopItFrontend/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GroceryStoreSearch from './components/GroceryStoreSearch';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GroceryStoreSearch />
     </View>
   );
 }
