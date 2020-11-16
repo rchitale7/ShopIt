85761f2f69615274120a781431711299319e151d
rchitale7 <36494864+rchitale7@users.noreply.github.com>
2020-11-15 22:47:02 -0800

    Crud api/rohan (#10)
    * get,add,delete functions for item and store tables
    
    * updated store schema so that it now embeds items, also updated API for items
    
    * forgot to delete some comments from commit fda2bfe, deleted them in this commit
    
    Co-authored-by: Rohan Chitale <rohanchitale@Rohans-MacBook-Pro.local>

--- ShopItBackend/models/store.model.js
+++ ShopItBackend/models/store.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
-<<<<<<< HEAD
 
 const aisleSchema = new schema({
     number: {
@@ -107,51 +106,6 @@ const storeSchema = new schema({
     items: {
         type: [itemSchema], 
         required: true
-=======
-const aisleSchema = new schema({
-    number: {
-        type: Number
-    },
-    xPos: {
-        type: Number
-    }, 
-    yPos: {
-        type: Number
-    }, 
-    length: {
-        type: Number
-    }, 
-    width: {
-        type: Number
-    }, 
-    sectorWidth: {
-        type: Number
-    }, 
-    sectorLength: {
-        type: Number
-    },
-    name: {
-        type: String
-    }, 
-    rotation: {
-        type: mongoose.Decimal128
-    }
-
-});
-const storeSchema = new schema({
-
-    name: {
-        type: String
-    },
-    long: {
-        type: mongoose.Decimal128
-    }, 
-    lat: {
-        type: mongoose.Decimal128
-    },
-    aisles: {
-        type: [aisleSchema]
->>>>>>> main
     }
 }, {
     timestamps: true
