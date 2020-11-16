5537134c0bc592f4645b1653193573feb162113c
Maggie Shi <maggieshi6@gmail.com>
2020-11-16 00:23:11 -0800

    added search autocomplete

--- ShopItFrontend/components/GroceryStoreSearch.js
+++ ShopItFrontend/components/GroceryStoreSearch.js
@@ -1,84 +1,172 @@
 import React, { useState, useEffect } from 'react';
-import {Text, View, StyleSheet, TextInput, FlatList, TouchableOpacity, Image } from 'react-native';
+import {Text, View, StyleSheet, TextInput, FlatList, TouchableOpacity, Image, TouchableHighlight } from 'react-native';
 import * as Location from 'expo-location';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { AppLoading } from 'expo';
 import { SearchBar } from 'react-native-elements';
 import {useFonts, ComicNeue_400Regular } from '@expo-google-fonts/comic-neue';
 import logo_filled from '../assets/logo_filled.png'
-const API_KEY = 'AIzaSyB0OBBZB0abirvfDAjbAWbCeGqk-knKvtw';
+
+const API_KEY='AIzaSyB0OBBZB0abirvfDAjbAWbCeGqk-knKvtw';
 
 const GroceryStoreSearch = () => {
     let [fontsLoaded] = useFonts({ComicNeue_400Regular});
-    const [location, setLocation] = useState(null);
-    const [search, setSearch] = useState(' ');
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
+    const [search, setSearch] = useState('');
     const [stores, setStores] = useState([]);
+    const [predictions, setPredictions] = useState([]);
     const [errorMsg, setErrorMsg] = useState(null);
 
     // if (!fontsLoaded) {
     //     return <AppLoading />;
     // }
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
+    async function getLocation() {
+        let { status } = await Location.requestPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+        }
+
+        var location = await Location.getLastKnownPositionAsync({});
+        // setLocation(location);
+
+        const latitude = location.coords.latitude;
+        setLatitude(latitude);
+        const longitude = location.coords.longitude;
+        setLongitude(longitude);
+    }
+
+    function locateStores() {
+        const placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&rankby=distance&keyword=grocery store&key=${API_KEY}`;
+    
+        const stores = [];
+
+        fetch(placeUrl)
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            for (let googlePlace of res.results) {
+                var store = {};
+
+                var placeId = googlePlace.place_id;
+                
+                const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=${API_KEY}`;
+                
+                store.name = googlePlace.name;
+                store.url = geocodeUrl;
+
+                stores.push(store);
             }
+        })
+        .then(res => {
+            return Promise.all(stores.map((store, i) => 
+                fetch(store.url)
+                .then(res => res.json()
+                .then(res => {
+                    var address = res.results[0].formatted_address;
+                    store.address = address;
+                }))))
+        })
+        .then(res => {
+            console.log(stores)
+            // set top 10 stores
+            setStores(stores.slice(0,9));
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
 
-            var location = await Location.getLastKnownPositionAsync({});
-            setLocation(location);
+    // useEffect(() => {
+    //     (async () => {
+    //         let { status } = await Location.requestPermissionsAsync();
+    //         if (status !== 'granted') {
+    //             setErrorMsg('Permission to access location was denied');
+    //         }
 
-            const latitude = location.coords.latitude;
-            const longitude = location.coords.longitude;
-    
-            const placeUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&rankby=distance&keyword=grocery store&key=' + API_KEY;
-    
-            const stores = [];
+    //         var location = await Location.getLastKnownPositionAsync({});
+    //         // setLocation(location);
 
-            fetch(placeUrl)
-            .then(res => {
-                return res.json()
-            })
-            .then(res => {
-                for (let googlePlace of res.results) {
-                    var store = {};
+    //         const latitude = location.coords.latitude;
+    //         setLatitude(latitude);
+    //         const longitude = location.coords.longitude;
+    //         setLongitude(longitude);
+
+    //         const placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&rankby=distance&keyword=grocery store&key=${API_KEY}`;
     
-                    var placeId = googlePlace.place_id;
+    //         const stores = [];
+
+    //         fetch(placeUrl)
+    //         .then(res => {
+    //             return res.json()
+    //         })
+    //         .then(res => {
+    //             for (let googlePlace of res.results) {
+    //                 var store = {};
+
+    //                 var placeId = googlePlace.place_id;
                     
-                    const geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?place_id=' + placeId + '&key=' + API_KEY;
+    //                 const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=${API_KEY}`;
                     
-                    store.name = googlePlace.name;
-                    store.url = geocodeUrl;
+    //                 store.name = googlePlace.name;
+    //                 store.url = geocodeUrl;
 
-                    stores.push(store);
-                    console.log(store)
-                }
-            })
-            .then(res => {
-                return Promise.all(stores.map((store, i) => 
-                    fetch(store.url)
-                    .then(res => res.json()
-                    .then(res => {
-                        var address = res.results[0].formatted_address;
-                        store.address = address;
-                    }))))
-            })
-            .then(res => {
-                console.log(stores)
-                // set top 10 stores
-                setStores(stores.slice(0,9));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        })()
-    }, []);
+    //                 stores.push(store);
+    //             }
+    //         })
+    //         .then(res => {
+    //             return Promise.all(stores.map((store, i) => 
+    //                 fetch(store.url)
+    //                 .then(res => res.json()
+    //                 .then(res => {
+    //                     var address = res.results[0].formatted_address;
+    //                     store.address = address;
+    //                 }))))
+    //         })
+    //         .then(res => {
+    //             console.log(stores)
+    //             // set top 10 stores
+    //             setStores(stores.slice(0,9));
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    //     })()
+    // }, []);
 
-    const renderItem = ({item}) => {
-        console.log("Creating item " + item.name);
+    function onChangeDestination(search) {
+        setSearch(search);
+
+        const autocompleteUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${search}&location=${latitude},${longitude}&radius=50&key=${API_KEY}`;
+    
+        fetch(autocompleteUrl)
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            var predictions = res.predictions;
+            setPredictions(predictions);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    function renderSuggestion(item) {
+        return (
+            <TouchableHighlight styles={styles.highlight} 
+            onPress={() => console.log("suggestion pressed")}>
+                <Text style={styles.suggestions}>{item.item.description}</Text>
+            </TouchableHighlight>
+        )
+    }
+
+    const renderButton = ({item}) => {
         return (
             <View style={styles.item}>
-                <TouchableOpacity style={styles.button} onPress={() => console.log("button presseed")}>
+                <TouchableOpacity style={styles.button} onPress={() => console.log("button pressed")}>
                     <Text style={styles.title}>{item.name}</Text>
                     <Text style={styles.descripton}>{item.address}</Text>
                 </TouchableOpacity>
@@ -86,25 +174,23 @@ const GroceryStoreSearch = () => {
         );
      };
 
-
     return (
         <View style={styles.container}>
             <Image style={styles.image} source={logo_filled}/>
-            <SearchBar
-            round
-            searchIcon={{ size: 24 }}
-            lightTheme = "true"
-            // onChangeText={setSearch(search)}
-            // value={search}
+            <TextInput style={styles.input}
+            placeholder="Search"
+            onChangeText={search => onChangeDestination(search)}
+            value={search}
             />
-            {/* <TextInput 
-            placeholder='Search for stores'
-            onChangeText={text => setSearch(text)}
-            /> */}
             <FlatList
+            data={predictions}
+            renderItem={item => renderSuggestion(item)}
+            keyExtractor={(item, index) => index.toString()}
+            />
+            <FlatList style={styles.list}
             data={stores}
-            renderItem={renderItem}
-            keyExtractor={(item, index) => index.toString() }
+            renderItem={renderButton}
+            keyExtractor={(item, index) => index.toString()}
             />
         </View>
     );
@@ -115,12 +201,35 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#91E78C',
-    //   alignItems: 'center',
       justifyContent: 'flex-start',
       paddingTop: 50,
     },
     image: {
         alignSelf: 'center',
+    },
+    suggestions: {
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 5,
+        fontSize: 18,
+        backgroundColor: '#fff',
+        fontFamily:"ComicNeue_400Regular"
+    },
+    input: {
+        height: 40,
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 5,
+        fontSize: 24,
+        backgroundColor: '#fff',
+        fontFamily:"ComicNeue_400Regular"
+    },
+    highlight: {
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    list: {
+        paddingTop: 75,
     },
     button: {
         alignItems: "flex-start",
