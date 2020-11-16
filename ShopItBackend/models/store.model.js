fda2bfe862723d70d716d133560b979a097077bd
rchitale7 <36494864+rchitale7@users.noreply.github.com>
2020-11-15 22:36:22 -0800

    Crud api/rohan (#9)
    * get,add,delete functions for item and store tables
    
    * updated store schema so that it now embeds items, also updated API for items
    
    Co-authored-by: Rohan Chitale <rohanchitale@Rohans-MacBook-Pro.local>

--- ShopItBackend/models/store.model.js
+++ ShopItBackend/models/store.model.js
@@ -1,6 +1,113 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
+<<<<<<< HEAD
+
+const aisleSchema = new schema({
+    number: {
+        type: Number, 
+        required: true
+    },
+    xPos: {
+        type: Number,
+        required: true
+    }, 
+    yPos: {
+        type: Number, 
+        required: true
+    }, 
+    length: {
+        type: Number, 
+        required: true
+    }, 
+    width: {
+        type: Number, 
+        required: true
+    }, 
+    sectorWidth: {
+        type: Number, 
+        required: true
+    }, 
+    sectorLength: {
+        type: Number, 
+        required: true
+    },
+    name: {
+        type: String, 
+        required: true
+    }, 
+    rotation: {
+        type: mongoose.Decimal128, 
+        required: true
+    }
+
+});
+
+const itemSchema = new schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    price: {
+        type: mongoose.Decimal128,
+        required: true
+
+    },
+    imageURL: {
+        type: String,
+        trim: true,
+        minlength: 1
+    }, 
+    size: {
+        type: String,
+        required: true
+    }, 
+    aisleNumber: {
+        type: Number, 
+        required: true
+    }, 
+    sectorX: {
+        type: Number,
+        required: true
+    }, 
+    sectorY: {
+        type: Number, 
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const storeSchema = new schema({
+
+    name: {
+        type: String, 
+        required: true
+    },
+    long: {
+        type: mongoose.Decimal128, 
+        required: true
+    }, 
+    lat: {
+        type: mongoose.Decimal128, 
+        required: true
+    },
+    aisles: {
+        type: [aisleSchema], 
+        required: true
+    }, 
+    items: {
+        type: [itemSchema], 
+        required: true
+=======
 const aisleSchema = new schema({
     number: {
         type: Number
@@ -44,6 +151,7 @@ const storeSchema = new schema({
     },
     aisles: {
         type: [aisleSchema]
+>>>>>>> main
     }
 }, {
     timestamps: true
--- ShopItBackend/routes/items.js
+++ ShopItBackend/routes/items.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-let Item = require('../models/item.model');
+let Store = require('../models/store.model');
 
+
+
+<<<<<<< HEAD
+// Return items for a specific grocery store
+router.route('/').get((req, res) => {
+    let object_id = req.query.id; 
+    Store.findOne({_id: object_id})
+    .then(store => res.json(store["items"]))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// Add items in bulk
+// TODO: add logging 
+router.route('/add').post((req, res) => {
+    let object_id = req.body.id;
+    let docs = req.body.data;
+    Store.updateMany({_id: object_id}, {
+        $push: {
+            items: {
+                $each: docs
+            }
+        }
+    }, function(err, response) {
+        if (err != null) {
+            res.status(400).json('Error: ' + err);
+        } else {
+            res.json(`"${docs.length}" items were added!`);
+        }
+    }); 
+});   
+
+
+
+// TODO: Delete items in bulk
+// TODO: add logging
+router.route('/delete').delete((req, res) => {
+    let object_id = req.body.id;
+    let docs = req.body.data
+    Store.updateMany({_id: object_id}, { $pull: { items: {_id: { $in: docs } }} }, function(err, response) {
+        if (err != null) {
+            res.status(400).json('Error: ' + err);
+        } else {
+            res.json(`"${docs.length}" items were deleted!`);
+        }
+    });   
+=======
 
 
 // Return items for a specific grocery store
@@ -59,6 +106,7 @@ router.route('/delete').delete((req, res) => {
         }
     });     
 
+>>>>>>> main
 });
 
 module.exports = router;
