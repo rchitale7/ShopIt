04417cb1f5b7ad6d30fd3388a3adc198668baa30
dvorjackz <dvorjackz@gmail.com>
2020-11-20 04:05:26 -0800

    Added pannable and zoomable map componenet

--- ShopItFrontend/components/map.js
+++ ShopItFrontend/components/map.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+
+import { View, Text, Image, Dimensions } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
+
 import SampleMap from '../assets/sample-map.png';
 
 function Map() {
     return (
-        <View>
-            <Image source={SampleMap} style={{width: 350, height: 600}}></Image>
-        </View>
+        <ImageZoom cropWidth={Dimensions.get('window').width}
+                       cropHeight={Dimensions.get('window').height}
+                       imageWidth={400}
+                       imageHeight={600}>
+            <Image source={SampleMap} style={{width: 400, height: 600}} />
+        </ImageZoom>
     );
 }
 
