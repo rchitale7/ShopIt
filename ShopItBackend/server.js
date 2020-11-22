3e483711f335dd38a75c1d79306811f4f950b1ad
Jack Zhang <dvorjackz@gmail.com>
2020-11-21 20:07:21 -0800

    Set up backend endpoint testing with Mocha and Chai (#16)
    * Set up Mocha
    
    * Made mocha work finally lmao
    
    * Add to CI/CD workflow
    
    * Force mocha to exit after running all tests
    
    * Fix mongoose unable to connect issue

--- ShopItBackend/server.js
+++ ShopItBackend/server.js
@@ -15,25 +15,25 @@ const uri = process.env.ATLAS_URI;
 mongoose.set('useFindAndModify', false);
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("Mongoose connection opened!");
-})
 
 const usersRouter = require('./routes/users');
 const itemsRouter = require('./routes/items');
 const storesRouter = require('./routes/stores');
 const aislesRouter = require('./routes/aisles');
 
-app.get('/', function (req, res) {
-    res.send('Welcome to ShopIt\'s API!');
-});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongoose connection opened!");
 
-app.use('/users', usersRouter);
-app.use('/items', itemsRouter);
-app.use('/stores', storesRouter);
-app.use('/aisles', aislesRouter);
+    app.listen(port, () => {
+        console.log(`This app is running on port ${port}!\n`);
+    }); 
 
-app.listen(port, () => {
-    console.log(`This app is running on port ${port}!`);
-}); 
+    app.get('/', (req, res) => { res.json('Welcome to ShopIt\'s API!') });
+    app.use('/users', usersRouter);
+    app.use('/items', itemsRouter);
+    app.use('/stores', storesRouter);
+    app.use('/aisles', aislesRouter);
+})
+
+module.exports = app;
---
+++ ShopItBackend/test/stores.js
@@ -0,0 +1,76 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = require('assert');
+const app = require('../server.js');
+
+const Store = require('../models/store.model');
+
+chai.use(chaiHttp);
+
+const exampleStore = {
+    "name": "Safeway",
+    "long": 100,
+    "lat": 100
+};
+
+const exampleStore2 = {
+    "name": "Trader Joe's",
+    "long": 200,
+    "lat": 200
+}
+
+describe('Stores', () => {
+    beforeEach(async () => { //Before each test we empty the database
+        await Store.deleteMany({});
+    });
+
+    describe('Store CRUD', () => {
+
+        it('Should return list of all stores', async () => {
+            let newStore1 = new Store(exampleStore);
+            let newStore2 = new Store(exampleStore2);
+
+            await newStore1.save();
+            await newStore2.save();
+
+            return await chai.request(app)
+                .get('/stores')
+                .then((res) => {
+                    assert.strictEqual(res.status, 200);
+                    assert.ok(Array.isArray(res.body));
+                    assert.strictEqual(res.body.length, 2);
+                })
+        });
+        
+        it('Should add a store', async () => {
+            return await chai.request(app)
+                .post('/stores/add')
+                .send({
+                    "name": "Safeway",
+                    "long": 100,
+                    "lat": 100
+                })
+                .then(async (res) => {
+                    assert.strictEqual(res.status, 200);
+                    let stores = await Store.find();
+                    assert.strictEqual(stores.length, 1);
+                })
+        });
+
+        it('Should delete a store', async () => {
+            let newStore = new Store(exampleStore);
+            let dbNewStore = await newStore.save();
+
+            return await chai.request(app)
+                .delete('/stores/delete')
+                .send({
+                    "id": dbNewStore._id
+                })
+                .then(async (res) => {
+                    assert.strictEqual(res.status, 200);
+                    let stores = await Store.find();
+                    assert.strictEqual(stores.length, 0);
+                })
+        });
+    });
+});
